2f8ed7b9ac51259fb449189a5aa29514961aa3f5
임상훈 <sanghoonim@hanyang.ac.kr>
2024-05-31 16:41:14 +0900

    [build]최초 폴더 설정 및 상단 네비게이션 바 빌드

---
+++ components/NavBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+
+import Link from "next/link";
+
+export function NavBar() {
+  return (
+    <div className="w-full h-[70px] flex items-center justify-center px-10">
+      <div className="text-black w-[1000px] text-2xl flex justify-between items-center gap-4">
+        <div className="flex justify-between gap-8">
+          <Link className="flex items-center gap-4" href="/">
+            <Image
+              src={"/logo.png"}
+              height={100}
+              width={100}
+              alt="logo"
+            ></Image>
+          </Link>
+          <input
+            type="text"
+            placeholder="가게 이름으로 찾아보세요."
+            className="bg-gray-200 w-[450px] h-[40px] rounded-lg px-5 text-[14px]"
+          ></input>
+        </div>
+
+        <div className="flex justify-between gap-5 text-[16px]">
+          <h1>로그인</h1>
+          <h1>회원가입</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
