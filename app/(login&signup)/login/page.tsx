8bba5ab67ca0b7d6b422487a4e31a1fe2a285ff1
임상훈 <sanghoonim@hanyang.ac.kr>
2024-06-02 02:35:27 +0900

    로그인 및 회원가입 아메일&비밀번호 유효성 검사 기능 구현 완료

--- app/(login&signup)/login/page.tsx
+++ app/(login&signup)/login/page.tsx
@@ -1,21 +1,60 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 export default function Page() {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateEmail = (email: string) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (validateEmail(value) == true || value == '') {
+      setEmailError('');
+    } else {
+      setEmailError('이메일 형식으로 작성해 주세요.');
+    }
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pw = e.target.value;
+    if (pw.length >= 8 || pw === '') {
+      setPasswordError('');
+    } else {
+      setPasswordError('비밀번호는 8자 이상이어야 합니다.');
+    }
+  };
+
   return (
     <div className="flex h-full justify-center flex-col items-center gap-10">
       <Image src={'/logo-big.png'} height={45} width={248} alt="logo"></Image>
       <form>
         <div className="flex flex-col gap-4">
-          <h1>이메일</h1>
-          <input
-            type="text"
-            placeholder="입력"
-            className="w-[350px] h-[58px] border-solid border-2 border-gray-200 rounded-md p-4 mb-3"></input>
-          <h1>비밀번호</h1>
-          <input
-            type="password"
-            placeholder="입력"
-            className="w-[350px] h-[58px] border-solid border-2 border-gray-200 rounded-md p-4 mb-4"></input>
+          <div>
+            <h1>이메일</h1>
+            <input
+              type="text"
+              placeholder="입력"
+              className="w-[350px] h-[58px] border-solid border-2 border-gray-200 rounded-md p-4 mb-3"
+              onChange={handleEmailChange}></input>
+            {emailError && <h1 className="text-red-500">{emailError}</h1>}
+          </div>
+
+          <div>
+            <h1>비밀번호</h1>
+            <input
+              type="password"
+              placeholder="입력"
+              className="w-[350px] h-[58px] border-solid border-2 border-gray-200 rounded-md p-4 mb-4"
+              onChange={handlePasswordChange}></input>
+            {passwordError && <h1 className="text-red-500">{passwordError}</h1>}
+          </div>
+
           <button className="w-[350px] h-[48px] bg-[#EA3C12] text-[16px] font-semibold rounded-md text-white">
             로그인하기
           </button>
