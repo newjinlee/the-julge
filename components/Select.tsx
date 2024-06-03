5fa1be709d7c00e2ec4a4c42276a12d70ca0bcf9
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-06-03 18:57:04 +0900

    [feat] 공통 인풋 컴포넌트 및 드롭다운 컴포넌트 생성

---
+++ components/Select.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import UpArrow from '@/public/dropdown-up.png';
+import DownArrow from '@/public/dropdown-down.png';
+import { useState } from 'react';
+
+interface SelectProps {
+  label: string;
+  options: string[];
+}
+
+export default function Select({ label, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 mb-8">
+      <label>{label}</label>
+      <div className="flex justify-between bg-white px-5 py-4  border border-solid border-gray-300 rounded-md">
+        <span className={`flex items-center ${selectedOption ? 'text-black' : 'text-gray-400'}`}>
+          {selectedOption || '선택'}
+        </span>
+        <button onClick={toggleDropdown}>
+          {isOpen ? (
+            <Image src={UpArrow} className="w-4 h-4" alt="UpArrow" />
+          ) : (
+            <Image src={DownArrow} className="w-4 h-4" alt="DownArrow" />
+          )}
+        </button>
+      </div>
+      {isOpen && (
+        <ul className="shadow-md">
+          {options.map(option => (
+            <li key={option} className="py-3 bg-white hover:bg-gray-100 border-b-2 text-center">
+              <button className="w-full" onClick={() => handleOptionClick(option)}>
+                {option}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
--- components/StoreInfoForm.tsx
+++ components/StoreInfoForm.tsx
@@ -1,7 +1,31 @@
+import Image from 'next/image';
+import close_icon from '@/public/close-icon.png';
+import CustomInput from './CustomInput';
+import Select from './Select';
+
 export default function StoreInfoForm() {
+  //드롭다운 옵션
+  const categories = ['한식', '중식', '일식', '양식', '분식', '카페', '편의점', '기타'];
+  const addresses = ['서울시 종로구', '서울시 중구'];
+
   return (
-    <div className="relative max-w-[964px] h-full mx-auto px-0 md:py-[60px] md:px-5 sm:py-[60px] sm:px-[12px]">
-      <h1 className="font-bold text-2xl mb-7">가게 정보</h1>
+    <div className="bg-gray-50 min-h-screen">
+      <div className="relative max-w-[964px] h-full mx-auto px-5 py-[60px] md:py-[60px]">
+        <div className="flex items-center justify-between mb-7">
+          <h1 className="font-bold text-2xl">가게 정보</h1>
+          <button>
+            <Image src={close_icon} alt="닫기 버튼" className="w-8 h-8" />
+          </button>
+        </div>
+        <div>
+          <CustomInput label="가게 이름*" placeholder="입력" />
+          <Select label="분류*" options={categories} />
+          <Select label="주소*" options={addresses} />
+          <CustomInput label="상세주소*" placeholder="입력" />
+          <CustomInput label="기본 시급*" placeholder="입력" />
+          <CustomInput label="가게 설명*" type="textarea" placeholder="입력" />
+        </div>
+      </div>
     </div>
   );
 }
