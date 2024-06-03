b51166044367dcca20cccb86f2e6d14dfa48eb03
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-06-03 16:45:49 +0900

    [style] 가게 정보 상세 페이지 반응형 수정

---
+++ components/StoreInfoForm.tsx
@@ -0,0 +1,7 @@
+export default function StoreInfoForm() {
+  return (
+    <div className="relative max-w-[964px] h-full mx-auto px-0 md:py-[60px] md:px-5 sm:py-[60px] sm:px-[12px]">
+      <h1 className="font-bold text-2xl mb-7">가게 정보</h1>
+    </div>
+  );
+}
