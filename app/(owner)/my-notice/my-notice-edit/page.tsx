966285b6690adeb0560e1b82ce1b61a80a09d6c9
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-06-07 21:39:30 +0900

    fix:공고등록 css 수정

--- app/(owner)/my-notice/my-notice-edit/page.tsx
+++ app/(owner)/my-notice/my-notice-edit/page.tsx
@@ -3,19 +3,21 @@ import NoticeForm from './NoticeForm';
 import CustomTextarea from '@/components/CustomTextarea';
 
 const MyNoticeEdit = () => {
+  const formData = [];
+
   return (
     <div className="relative px-[12px] py-[60px] max-w-[964px] h-full mx-auto">
       <h1 className="font-bold text-2xl mb-7">공고등록</h1>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row flex-wrap gap-2">
         <div className="relative w-[308px]">
-          <CustomInput label="시급*" className="border px-[20px] py-[16px] h-[58px] w-[308px]" />
+          <CustomInput label="시급*" placeholder="0" className="border px-[20px] py-[16px] h-[58px] w-[308px]" />
           <p className="absolute top-[40%] right-3">원</p>
         </div>
         <div className="w-[308px]">
           <CustomInput label="시작 일시*" className="border px-[20px] py-[16px] h-[58px] w-[308px]" />
         </div>
         <div className="relative w-[308px]">
-          <CustomInput label="업무 시간*" className="border px-[20px] py-[16px] h-[58px] w-[308px]" />
+          <CustomInput label="업무 시간*" placeholder="0" className="border px-[20px] py-[16px] h-[58px] w-[308px]" />
           <p className="absolute top-[40%] right-3">시간</p>
         </div>
       </div>
