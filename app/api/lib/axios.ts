34d460568951d36356630d51a97152ea61a9f87f
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-06-12 11:11:55 +0900

    chore: axios baseURL 상수명 변경

--- app/api/lib/axios.ts
+++ app/api/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.THE_JULGE_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
   withCredentials: true,
