9a9e80bc20b5b5cdf4a3c1d49df2843e98bb1249
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-06-15 13:09:20 +0900

    fex: axios 파일 수정

--- app/api/lib/axios.ts
+++ app/api/lib/axios.ts
@@ -3,8 +3,30 @@ import axios, { AxiosInstance } from 'axios';
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 5000,
-  headers: { 'X-Custom-Header': 'foobar' },
-  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  response => {
+    return response;
+  },
+
+  error => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
